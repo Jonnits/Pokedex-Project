a2ad5d9e1e5367e29ccc214f700b871c3b831f30
Jonnits <jonathanrubra@gmail.com>
2024-11-25 15:36:57 -0800

    Created first 3 objects representing first 3 Pokemon
    Included the same keys across all: name, height, types, category, and abilities.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
 let pokemonList = [];
+
+let pokemon1 = {
+    name: 'Bulbasaur',
+    height: 2.04,
+    types: ['grass', 'poison'],
+    category: 'Seed',
+    abilities: 'Overgrow'
+}
+
+let pokemon2 = {
+    name: 'Ivysaur',
+    height: 3.03,
+    types: ['grass', 'poison'],
+    category: 'Seed',
+    abilities: 'Overgrow'
+}
+
+let pokemon3 = {
+    name: 'Venusaur',
+    height: 6.07,
+    types: ['grass', 'poison'],
+    category: 'Seed',
+    abilities: 'Overgrow'
+}
+
